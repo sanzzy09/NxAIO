9b8d32da57f43936084883991ecfbc381e3423a7
M.Ikhsan C.P <ikhsancandra721@gmail.com>
2026-06-11 06:18:50 +0000

    buat styling disesuaikan dan di improvisasi dengan theme color web (_for

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -187,7 +187,7 @@ export default function Home() {
                <Features6 />
             </div>
 
-            <div className="full-bleed-technical-section -mx-4 lg:-mx-24 bg-black">
+            <div className="full-bleed-technical-section -mx-4 lg:-mx-24 bg-primary">
                <Features4 />
             </div>
 
