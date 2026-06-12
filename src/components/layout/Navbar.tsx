8625ee2bd1dc292d7fa0bfff00c06e62f7b49d16
M.Ikhsan C.P <ikhsancandra721@gmail.com>
2026-06-12 17:33:28 +0000

    tinggal dinavbar yang perlu dikecilkan

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -54,12 +54,12 @@ export function Navbar({ onDashboardClick }: { onDashboardClick?: () => void }) 
           className="flex items-center group"
           onClick={onDashboardClick}
         >
-          <div className="w-36 h-10 md:w-48 md:h-12 relative group-hover:scale-105 transition-transform duration-300 flex items-center justify-center">
+          <div className="w-28 h-8 md:w-36 md:h-10 relative group-hover:scale-105 transition-transform duration-300 flex items-center justify-center">
             <Image 
               src={siteConfig.logo} 
               alt={siteConfig.name} 
-              width={192} 
-              height={48} 
+              width={144} 
+              height={40} 
               className="object-contain"
               priority
               unoptimized
